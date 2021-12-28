be6f8aa9e63c6f10d693e2a27ad9fe3a304cbb9c
Raven Ebonscale <CreateHavoc@hotmail.com>
2021-12-27 23:03:46 -0500

    Index

--- FurryClassifier/ArrayStuff.cs
+++ FurryClassifier/ArrayStuff.cs
@@ -10,10 +10,9 @@ namespace ArryStuff
         /// </summary>
         /// <param name="Arry">A Float Arry</param>
         /// <returns> Index</returns>
-        static public int ArgsMax(this object[] Arry)
+        static public int ArgsMax(this float[] Arry)
         {
-            if (Arry.GetType() == typeof(float[]))
-            {
+
                 float? maxVal = null; //nullable so this works even if you have all super-low negatives
                 int index = -1;
                 for (int i = 0; i < Arry.Length; i++)
@@ -29,31 +28,8 @@ namespace ArryStuff
 
 
                 return index;
-            }
-           else if(Arry.GetType() == typeof(int[]))
-            {
-                int? maxVal = null; //nullable so this works even if you have all super-low negatives
-                int index = -1;
-                for (int i = 0; i < Arry.Length; i++)
-                {
-                    int thisNum = (int)Arry[i];
+            
 
-                    if (!maxVal.HasValue|| thisNum > maxVal.Value)
-                    {
-                        maxVal = thisNum;
-                        index = i;
-                    }
-
-                }
-
-
-                return index;
-
-            }
-            else 
-            { 
-                throw new ArgumentException("Parameter must be Float[] or Int[]", nameof(Arry));
-            }
 
         }
 
